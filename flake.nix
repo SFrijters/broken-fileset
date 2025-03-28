574ca83951919bc8e6b0051c77d92b4bc70a0fa9
Stefan Frijters <sfrijters@gmail.com>
2025-03-28 14:06:42 +0100

    Clean up flake

--- flake.nix
+++ flake.nix
@@ -1,31 +1,27 @@
 {
-  description = "fileset bug";
+  description = "Reproducer code for https://github.com/NixOS/nixpkgs/issues/393845";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
   outputs =
     {
-      self,
       nixpkgs,
+      ...
     }:
     let
       system = "x86_64-linux";
-      name = "fileset-bug";
 
       pkgs = import nixpkgs {
         inherit system;
       };
     in
-      {
+    {
+      packages.${system} = {
+        pkg-direct-src = pkgs.callPackage ./common/mypkg-src { };
+        pkg-link-src = pkgs.callPackage ./link/mypkg-src { };
 
-        packages.${system} = {
-          pkg-direct-src = pkgs.callPackage ./common/mypkg-src { };
-          pkg-link-src = pkgs.callPackage ./link/mypkg-src { };
-
-          pkg-direct-fileset = pkgs.callPackage ./common/mypkg-fileset { };
-          pkg-link-fileset = pkgs.callPackage ./link/mypkg-fileset { };
-        };
+        pkg-direct-fileset = pkgs.callPackage ./common/mypkg-fileset { };
+        pkg-link-fileset = pkgs.callPackage ./link/mypkg-fileset { };
       };
+    };
 }
